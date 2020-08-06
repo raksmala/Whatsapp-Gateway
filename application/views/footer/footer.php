260bf7d8b1306b6edc3b56c1fddeaf7dfbd129fb
raksmala <raksmala@gmail.com>
2020-08-06 09:38:11 +0700

    UI2 06082020

--- application/views/footer/footer.php
+++ application/views/footer/footer.php
@@ -69,6 +69,50 @@
             no_results_text: "Oops, nothing found!",
             width: "100%"
         });
+        
+        $image_crop = $('#imageDemo').croppie({
+        enableExif: true,
+        viewport: {
+        width:250,
+        height:200,
+        type:'circle'
+        },
+        boundary:{
+        width:300,
+        height:300
+        }
+    });
+    
+    $('#uploadImage').on('change', function(){
+        var reader = new FileReader();
+        reader.onload = function (event) {
+        $image_crop.croppie('bind', {
+            url: event.target.result
+        }).then(function(){
+            console.log('jQuery bind complete');
+        });
+        }
+        reader.readAsDataURL(this.files[0]);
+        $('#uploadImageModal').modal('show');
+    });
+    
+    $('.crop_image').click(function(event){
+        $image_crop.croppie('result', {
+        type: 'canvas',
+        size: 'viewport'
+        }).then(function(response){
+        $.ajax({
+            url:"upload.php",
+            type: "POST",
+            data:{"image": response},
+            success:function(data)
+            {
+            $('#uploadImageModal').modal('hide');
+            $('#uploadedImage').html(data);
+            }
+        });
+        })
+    });
     });
 </script>
 </body>
